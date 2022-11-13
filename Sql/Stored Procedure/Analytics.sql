2c6b08bc98ed6190fc13c7422e1bcf2f180e1a7c
GKeSolutions <Gilbert.khoury@gkesolutions.com>
2022-11-13 21:09:37 +0200

    Fixing Null EndDateTime filters

--- Sql/Stored Procedure/Analytics.sql
+++ Sql/Stored Procedure/Analytics.sql
@@ -13,19 +13,19 @@ SELECT
 	SUM(DATEDIFF(
 				HOUR, 
 				Case When step.StartDateTime < @StartDate Then @StartDate Else step.StartDateTime End, 
-				Case When step.EndDateTime > @EndDate Then @EndDate Else step.EndDateTime End
+				Case When ISNULL(step.EndDateTime, GETDATE()) >= @EndDate Then @EndDate Else step.EndDateTime End
 				)
 		)/Count(1) AvgTimeSpentInhours, --Returns the average time spent per user per process
 	SUM(DATEDIFF(
 				MINUTE,
 				Case When step.StartDateTime < @StartDate Then @StartDate Else step.StartDateTime End, 
-				Case When step.EndDateTime > @EndDate Then @EndDate Else step.EndDateTime End
+				Case When ISNULL(step.EndDateTime, GETDATE()) >= @EndDate Then @EndDate Else step.EndDateTime End
 				)
 		)/Count(1) AvgTimeSpentInMinutes, --Returns the average time spent per user per process
 	SUM(DATEDIFF(
 				MINUTE,
 				Case When step.StartDateTime < @StartDate Then @StartDate Else step.StartDateTime End, 
-				Case When step.EndDateTime > @EndDate Then @EndDate Else step.EndDateTime End
+				Case When ISNULL(step.EndDateTime, GETDATE()) >= @EndDate Then @EndDate Else step.EndDateTime End
 				)) TotalTimeSpentInMinutes
 FROM 
 	nxfwkprocessitemstep step
@@ -38,9 +38,9 @@ WHERE
 	ObjectType.AppObjectTypeCode = 'Process'
 	--AND item.procitemid = '687EA340-B661-4E6C-B965-004B158091A9'
 	AND step.StepType <> 0 -- Exclude the "Change Owner" step
-	AND DATEDIFF(MINUTE, step.StartDateTime, step.EndDateTime) > 0
+	AND DATEDIFF(MINUTE, step.StartDateTime, ISNULL(step.EndDateTime, GETDATE())) > 0
 	And step.StartDateTime < @EndDate
-	And step.EndDateTime > @StartDate
+	And ISNULL(step.EndDateTime, GETDATE()) > @StartDate
 GROUP BY 
 	AppObject.AppObjectCode, 
 	msg.Msg,
@@ -51,14 +51,13 @@ HAVING
 	SUM(DATEDIFF(
 			HOUR, 
 			Case When step.StartDateTime < @StartDate Then @StartDate Else step.StartDateTime End, 
-			Case When step.EndDateTime > @EndDate Then @EndDate Else step.EndDateTime End
+			Case When ISNULL(step.EndDateTime, GETDATE()) >= @EndDate Then @EndDate Else step.EndDateTime End
 			)
 	)/Count(1) > 0.1
 ORDER BY currentUserid, SUM(DATEDIFF(
 			MINUTE, 
 			Case When step.StartDateTime < @StartDate Then @StartDate Else step.StartDateTime End, 
-			Case When step.EndDateTime > @EndDate Then @EndDate Else step.EndDateTime End
+			Case When ISNULL(step.EndDateTime, GETDATE()) >= @EndDate Then @EndDate Else step.EndDateTime End
 			)
 	)/Count(1) desc
 END
-
