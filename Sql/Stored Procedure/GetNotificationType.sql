7532615c61d12b2101b35912093ab14b17d7fa3f
Joe Doumit <joe.doumit@wilsonallen.com>
2022-12-16 13:42:43 +0200

    Added NotificationType sp

---
+++ Sql/Stored Procedure/GetNotificationType.sql
@@ -0,0 +1,9 @@
+IF OBJECT_ID('[ed].[GeNotificationType]','P') IS NULL
+	EXEC('CREATE PROCEDURE [ed].[GeNotificationType] WITH ENCRYPTION AS BEGIN SELECT NULL; END;');
+GO
+ALTER PROCEDURE [ed].[GeNotificationType]
+AS
+BEGIN
+    SET NOCOUNT ON;
+	SELECT Code, [Description] FROM ed.NotificationType;
+END
