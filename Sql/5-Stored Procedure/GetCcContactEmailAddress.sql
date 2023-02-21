7cbdcbfb6d0e3f4b402736d230aae83aa09d7f4c
joe.doumit@intapp.com <joe.doumit@intapp.com>
2023-02-21 14:36:16 +0200

    fix

--- Sql/5-Stored Procedure/GetCcContactEmailAddress.sql
+++ Sql/5-Stored Procedure/GetCcContactEmailAddress.sql
@@ -2,7 +2,7 @@ IF OBJECT_ID('[ed].[GetCcContactEmailAddress]','P') IS NULL
 	EXEC('CREATE PROCEDURE [ed].[GetCcContactEmailAddress] WITH ENCRYPTION AS BEGIN SELECT NULL; END;');
 GO
 ALTER PROCEDURE [ed].[GetCcContactEmailAddress]
-	@ccContact uniqueidentifier
+	@ccContact NVARCHAR(200)
 AS
 BEGIN
 	SELECT n.EmailAddr From NxFWKUser n
