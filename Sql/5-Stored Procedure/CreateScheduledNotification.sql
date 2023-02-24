31b1206cdb405dc7b0730c15a33830e6ef2b9d72
joe.doumit@intapp.com <joe.doumit@intapp.com>
2023-02-24 12:33:22 +0200

    fix

--- Sql/5-Stored Procedure/CreateScheduledNotification.sql
+++ Sql/5-Stored Procedure/CreateScheduledNotification.sql
@@ -15,7 +15,7 @@ BEGIN
 	BEGIN TRY
 		Declare @EventType INT, @CronExpression NVARCHAR(200);
 		SELECT @EventType = nt.[Type] FROM ed.NotificationTemplate nt WHERE Id = @NotificationTemplate;
-		SELECT @CronExpression = s.CronExpression FROM ed.Scheduler s
+		SELECT @CronExpression = s.CronExpression FROM ed.Scheduler s WHERE Id = @Scheduler
 		INSERT INTO [ed].[ScheduledNotification]
 				([IsActive]
 				,[NotificationTemplate]
