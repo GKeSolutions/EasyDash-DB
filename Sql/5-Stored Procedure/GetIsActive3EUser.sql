0f27939f7761ea6e47b6f88ec765f0f26110ff2d
GKeSolutions <Gilbert.khoury@gkesolutions.com>
2023-03-08 18:54:06 +0200

    .

--- Sql/5-Stored Procedure/GetIsActive3EUser.sql
+++ Sql/5-Stored Procedure/GetIsActive3EUser.sql
@@ -1,4 +1,4 @@
-OBJECT_ID('[ed].[GetIsActive3EUser]','P') IS NULL
+If OBJECT_ID('[ed].[GetIsActive3EUser]','P') IS NULL
 	EXEC('CREATE PROCEDURE [ed].[GetIsActive3EUser] WITH ENCRYPTION AS BEGIN SELECT NULL; END;');
 GO
     ALTER PROCEDURE [ed].[GetIsActive3EUser]
@@ -10,7 +10,7 @@ GO
 		SELECT @IsActive3EUser = ISNULL(baseUser.IsActive, 0)
 		FROM dbo.NxFWKUser fwkUser 
 		JOIN dbo.NxBaseUser baseUser ON baseUser.NxBaseUserID = fwkUser.NxFWKUserID
-		WHERE fwkUser.NetworkAlias = 
+		WHERE fwkUser.NetworkAlias = @NetworkAlias
 
 		SELECT @IsActive3EUser IsActive3EUser
 END
