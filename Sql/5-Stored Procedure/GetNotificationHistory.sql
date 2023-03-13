3c389f58e724231f0c51d2d1a66c20fdf3f419db
GKeSolutions <Gilbert.khoury@gkesolutions.com>
2023-03-13 15:26:07 +0200

    .

--- Sql/5-Stored Procedure/GetNotificationHistory.sql
+++ Sql/5-Stored Procedure/GetNotificationHistory.sql
@@ -27,6 +27,7 @@ BEGIN
 		LEFT OUTER JOIN dbo.NxBaseUser reassignUser ON reassignUser.NxBaseUserId = hist.Reassignto
 		LEFT OUTER JOIN dbo.NxBaseUser triggeredByUser ON triggeredByUser.NxBaseUserId = hist.TriggeredBy
 		WHERE (@UserId IS NULL OR hist.UserId = @UserId) AND (@ProcessCode IS NULL OR hist.ProcessCode = @ProcessCode) AND (@ProcItemId IS NULL OR hist.ProcItemId = @ProcItemId) AND EventType=1 AND hist.InsertDate > @StartDate AND hist.InsertDate < @EndDate
+		Order by hist.InsertDate desc, baseUser.BaseUserName
 	ELSE IF @ActionType = 2
 		SELECT
 			baseUser.BaseUserName UserName
@@ -42,5 +43,6 @@ BEGIN
 		JOIN dbo.NxBaseUser BaseUser ON baseUser.NxBaseUserID = hist.UserId
 		LEFT OUTER JOIN dbo.NxBaseUser triggeredByUser ON triggeredByUser.NxBaseUserId = hist.TriggeredBy
 		WHERE (@UserId IS NULL OR hist.UserId = @UserId) AND EventType=2 AND hist.InsertDate > @StartDate AND hist.InsertDate < @EndDate
+		Order by hist.InsertDate desc, baseUser.BaseUserName
 END
 GO
